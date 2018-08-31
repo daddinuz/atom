33b67fbe3b6d4bff5fcaf08bfe89dce1f6242ecc
daddinuz <daddinuz@gmail.com>
2018-08-31 15:51:49 +0200

    update dependencies

--- sources/atom.c
+++ sources/atom.c
@@ -1,29 +1,28 @@
 /*
- * Author: daddinuz
- * email:  daddinuz@gmail.com
- *
- * Copyright (c) 2018 Davide Di Carlo
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
+Author: daddinuz
+email:  daddinuz@gmail.com
+
+Copyright (c) 2018 Davide Di Carlo
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #include <stdio.h>
@@ -69,13 +68,13 @@ static struct Atom_Node *table[ATOM_TABLE_SIZE] = {0};
  *
  */
 Atom Atom_fromBytes(const void *const bytes, const size_t length) {
-    assert(bytes);
+    assert(NULL != bytes);
     assert(length < SIZE_MAX);
     return Atom_put(bytes, length, Atom_hash(bytes, length))->bytes;
 }
 
 Atom Atom_fromLiteral(const char *const literal) {
-    assert(literal);
+    assert(NULL != literal);
     assert(strlen(literal) < SIZE_MAX);
     return Atom_fromBytes(literal, strlen(literal));
 }
@@ -97,15 +96,15 @@ Atom Atom_fromFloating(const long double number) {
 }
 
 size_t Atom_length(Atom atom) {
-    assert(atom);
+    assert(NULL != atom);
     Atom_assertValidInstance(atom);
     struct Atom_Node *node = ((struct Atom_Node *) atom) - 1;
     return node->length;
 }
 
 bool Atom_equals(Atom atom, Atom other) {
-    assert(atom);
-    assert(other);
+    assert(NULL != atom);
+    assert(NULL != other);
     Atom_assertValidInstance(atom);
     Atom_assertValidInstance(other);
     return atom == other;
@@ -115,7 +114,7 @@ bool Atom_equals(Atom atom, Atom other) {
  *
  */
 struct Atom_Node *Atom_Node_new(const void *const bytes, const size_t length, const uint32_t hash) {
-    assert(bytes);
+    assert(NULL != bytes);
     assert(length < SIZE_MAX);
     struct Atom_Node *self = Option_unwrap(Alligator_malloc(length + 1));
     memcpy(self->bytes, bytes, length);
@@ -141,7 +140,7 @@ uint32_t Atom_hash(const void *const bytes, const size_t length) {  /* Jenkins o
 }
 
 struct Atom_Node *Atom_put(const void *const bytes, const size_t length, const uint32_t hash) {
-    assert(bytes);
+    assert(NULL != bytes);
     assert(length < SIZE_MAX);
     struct Atom_Node *node;
 
@@ -169,7 +168,7 @@ struct Atom_Node *Atom_put(const void *const bytes, const size_t length, const u
 }
 
 struct Atom_Node *Atom_fetch(const void *const bytes, const size_t length, const uint32_t hash) {
-    assert(bytes);
+    assert(NULL != bytes);
     assert(length < SIZE_MAX);
     const size_t index = hash % ATOM_TABLE_SIZE;
 
@@ -199,7 +198,7 @@ void Atom_onExit(void) {
 }
 
 void Atom_assertValidInstance(Atom atom) {
-    assert(atom);
+    assert(NULL != atom);
     (void) atom;
 #ifndef NDEBUG
     struct Atom_Node *node = ((struct Atom_Node *) atom) - 1;
